9da409c720fdaf649c69968ff7bd67e30185a613
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-10-12 15:39:37 -0400

    this isn't even remotely organized, just, whatever

--- app/router.js
+++ app/router.js
@@ -6,13 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('group', {path:'/group/:group_id'});
-  this.route('user', {path:'user/:user_id'});
-  this.route('post', {path:'post/:post_id'});
-  this.route('game', {path:'game/:game_id'});
-  this.route('matchrequest', {path:'match/:match_id'});
-this.route('game',{},function(){
-    this.route('matchmaking');
+	this.route('group', {path:'/group/:group_id'});
+	this.route('user', {path:'/user/:user_id'});
+	this.route('post', {path:'/post/:post_id'});
+	this.route('game', {path:'/game/:game_id'});
+	this.route('matchrequest', {path:'/match/:match_id'});
 });
 
 export default Router;
