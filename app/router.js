4c5f3528931e16fcbff99a8921d03ba03f417f60
unknown <wilson.velez1@upr.edu>
2015-10-13 19:45:13 -0400

    commit just to pull

--- app/router.js
+++ app/router.js
@@ -12,14 +12,14 @@ Router.map(function() {
   //this.route('post', {path:'post/:post_id'});
   //this.route('game');
     this.route('grouplist');
-    this.route('group/:group_id');
+    this.route('group',{path:'/group/:group_id'});
     this.route('gamelist');
   //this.route('matchrequest', {path:'match/:match_id'});
     //this.route('group', {path:'/group/:group_id'});
-    this.route('user', {path:'/user/:user_id'});
+    this.route('user');
     this.route('post', {path:'/post/:post_id'});
     this.route('game', {path:'/game/:game_id'});
-    this.route('matchrequest', {path:'/match/:match_id'});
+    this.route('matchmaking');
 });
 
 export default Router;
