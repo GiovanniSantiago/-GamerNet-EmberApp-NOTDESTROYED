9ff23e03c883b79a283e441bf7dbf23d16b066a6
unknown <wilson.velez1@upr.edu>
2015-10-13 19:57:37 -0400

    13/10/15 7:57pm

--- app/router.js
+++ app/router.js
@@ -16,7 +16,7 @@ Router.map(function() {
     this.route('gamelist');
   //this.route('matchrequest', {path:'match/:match_id'});
     //this.route('group', {path:'/group/:group_id'});
-    this.route('user');
+    this.route('user',{path:'/user/:user_id'});
     this.route('post', {path:'/post/:post_id'});
     this.route('game', {path:'/game/:game_id'});
     this.route('matchmaking');
