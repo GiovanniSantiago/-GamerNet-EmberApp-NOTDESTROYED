ce1f8607b1a6609bc70f9a45e5f50d96b29fe9e8
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-02 22:01:56 -0400

    Testing requests

--- app/router.js
+++ app/router.js
@@ -6,21 +6,20 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-
-
   //this.route('group', {path:'/group/:group_id'});
   //this.route('post', {path:'post/:post_id'});
   //this.route('game');
-    this.route('grouplist');
-    this.route('group',{path:'/group/:group_id'});
-    this.route('gamelist');
-    this.route('eventlist');
+  this.route('grouplist');
+  this.route('group',{path:'/group/:group_id'});
+  this.route('gamelist');
+  this.route('eventlist');
   //this.route('matchrequest', {path:'match/:match_id'});
-    //this.route('group', {path:'/group/:group_id'});
-    this.route('user',{path:'/user/:user_id'});
-    this.route('post', {path:'/post/:post_id'});
-    this.route('game', {path:'/game/:game_id'});
-    this.route('matchmaking');
+  //this.route('group', {path:'/group/:group_id'});
+  this.route('user',{path:'/user/:user_id'});
+  this.route('post', {path:'/post/:post_id'});
+  this.route('game', {path:'/game/:game_id'});
+  this.route('matchmaking');
+  this.route('postTest', {});
 });
 
 export default Router;
