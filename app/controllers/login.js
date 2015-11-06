0495d55234b2fdb45adda6a18f58badf583f27c5
wilx94 <wilson.velez1@upr.edu>
2015-11-05 20:31:50 -0400

    akjdhaskjdf

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,7 +6,7 @@ export default Ember.Controller.extend({
 	password:"password",
 	actions:{
 		validateUser(){
-			Ember.$.post("//gamer-net.herokuapp.com/json/validateUser",{"email": "this.email","password":"this.password"}).then(function(data){
+			Ember.$.post("//api-gamer-net.herokuapp.com/json/validateUser",{"email": "this.email","password":"this.password"}).then(function(data){
                 
                     alert("validate user. "+data);
               
