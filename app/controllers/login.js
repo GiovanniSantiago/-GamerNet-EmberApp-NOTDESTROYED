da44fed38a91e609f953f9af0759b91a1ba9e6a8
wilx94 <wilson.velez1@upr.edu>
2015-11-06 21:05:36 -0400

    sync

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,32 +3,43 @@ import Ajax from 'ic-ajax';
 
 export default Ember.Controller.extend({
 
-	email:"",
-	password:"",
+	validate_email:"",
+	validate_password:"",
+    first_name:"",
+    middle_name:"",
+    last_name:"",
+    email:"",
+    user_name:"",
+    password:"",
+    country:"",
+    date_of_birth:"",
     validate:true,
 	actions:{
 		validateUser(){
-            var data = { "email" : this.email,"password":this.password };
-Ember.$.ajax({
-    type: "POST",
-    url: "//api-gamer-net.herokuapp.com/json/validateUser",
-    processData: false,
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-    success: function(result) {
-        console.log(result);
+            var data = { "email" : this.validate_email,"password":this.validate_password };
 
-}});
+            Ember.$.ajax({
+                type: "POST",
+                url: "//api-gamer-net.herokuapp.com/json/validateUser",
+                processData: false,
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function(result) {
+                    console.log(result);
+
+                }
+            });
             
-            ////
-//			Ember.$.post("//api-gamer-net.herokuapp.com/json/validateUser",JSON.stringify({"email": "this.email","password":"this.password"})).then(function(data){
-//                
-//                    console.log("validate user. "+data);
-//              
-//            },function(data){
-//                console.log("validate user fails."+JSON.stringify(data));
-//            });
-
-		}
+        },
+        createUser(){
+            var data = {"first_name":this.first_name,
+    "middle_name":this.middle_name,
+    "last_name":this.last_name,
+    "email":this.email,
+    "user_name":ths.user_name,
+    "password":this.password,
+    "country":this.country,
+    "date_of_birth":this.date_of_birth}
+        }
 	}
 });
