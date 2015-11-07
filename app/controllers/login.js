20d0bd2982a570553d2fc89679af1bed8d752bed
wilx94 <wilson.velez1@upr.edu>
2015-11-06 23:38:14 -0400

    test

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -32,30 +32,6 @@ export default Ember.Controller.extend({
                             }
             });
             
-        },
-        createUser(){
-                    var data = {"first_name":this.first_name,
-                                "middle_name":this.middle_name,
-                                "last_name":this.last_name,
-                                "email":this.email,
-                                "user_name":this.user_name,
-                                "password":this.password,
-                                "country":this.country,
-                                "date_of_birth":this.date_of_birth};
-
-                    Ember.$.ajax({
-                                    type: "POST",
-                                    url: "//api-gamer-net.herokuapp.com/json/user",
-                                    processData: false,
-                                    contentType: 'application/json',
-                                    data: JSON.stringify(data),
-                                    success: function(result) {
-
-                                        console.log(result);
-
-                                    }
-                    });
-     
         }
 	}
 });
