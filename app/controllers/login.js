9d3cb08a53cce34412d38c52cec40d4049d05f59
wilx94 <wilson.velez1@upr.edu>
2015-11-06 23:16:37 -0400

    user register

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -19,27 +19,42 @@ export default Ember.Controller.extend({
             var data = { "email" : this.validate_email,"password":this.validate_password };
 
             Ember.$.ajax({
-                type: "POST",
-                url: "//api-gamer-net.herokuapp.com/json/validateUser",
-                processData: false,
-                contentType: 'application/json',
-                data: JSON.stringify(data),
-                success: function(result) {
-                    console.log(result);
+                            type: "POST",
+                            url: "//api-gamer-net.herokuapp.com/json/validateUser",
+                            processData: false,
+                            contentType: 'application/json',
+                            data: JSON.stringify(data),
+                            success: function(result) {
+                            
+                                console.log(result);
 
-                }
+                            }
             });
             
         },
         createUser(){
             var data = {"first_name":this.first_name,
-    "middle_name":this.middle_name,
-    "last_name":this.last_name,
-    "email":this.email,
-    "user_name":ths.user_name,
-    "password":this.password,
-    "country":this.country,
-    "date_of_birth":this.date_of_birth}
+                        "middle_name":this.middle_name,
+                        "last_name":this.last_name,
+                        "email":this.email,
+                        "user_name":ths.user_name,
+                        "password":this.password,
+                        "country":this.country,
+                        "date_of_birth":this.date_of_birth};
+            
+            Ember.$.ajax({
+                            type: "POST",
+                            url: "//api-gamer-net.herokuapp.com/json/user",
+                            processData: false,
+                            contentType: 'application/json',
+                            data: JSON.stringify(data),
+                            success: function(result) {
+                           
+                                console.log(result);
+
+                            }
+            });
+            
         }
 	}
 });
