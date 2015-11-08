84e295052063bf7d7849835d6dbbbbd4d1256348
wilx94 <wilson.velez1@upr.edu>
2015-11-08 18:25:15 -0400

    testing findAll(group)

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -53,8 +53,7 @@ export default Ember.Controller.extend({
                             contentType: 'application/json',
                             data: JSON.stringify(data),
                             success: function(result) {
-                                this.validate_email=result.user_id;
-                                this.validate_password=result.password;
+                                this.set('validate_user',result.user_id);
                                 console.log(JSON.stringify(result));
                                 console.log(JSON.stringify(result.user_id)+" "+JSON.stringify(result.password));
                                 console.log(this.validate_email);
