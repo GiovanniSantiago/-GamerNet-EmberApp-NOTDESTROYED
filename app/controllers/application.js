f8e354856d05ac7a12379b9eef13f2d8ee9f9a82
wilx94 <wilson.velez1@upr.edu>
2015-11-07 20:59:36 -0400

    user get information

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,30 @@
 import Ember from 'ember';  
 
 export default Ember.Controller.extend({
-    firstName: 'Wilson',
-    lastName: 'Velez'
+     user_id:1,
+    first_name:"",
+    middle_name:"",
+    last_name:"",
+    email:"",
+    user_name:"",
+    country:"",
+    date_of_birth:"",
+    author_id:"",
+    post_owner_id:"",
+    avatar_id:"",
+    actions:{
+        readUserInformation(){
+            Ember.$.ajax({
+                            url: "//api-gamer-net.herokuapp.com/json/user/"+user_id,
+                            processData:false,
+                            contentType:'application/json',                
+                            success: function(result){
+                                console.log(result);
+                            },
+                            error: function(result){
+                                console.log(result);
+                            }
+});
+        }
+    }
 });
