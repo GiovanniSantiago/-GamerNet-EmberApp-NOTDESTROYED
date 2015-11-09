8049b74408e543aa80d9d00a47a40532c6d36c05
wilx94 <wilson.velez1@upr.edu>
2015-11-09 18:40:01 -0400

    testing change page with login.js

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -54,7 +54,7 @@ export default Ember.Controller.extend({
                             data: JSON.stringify(data),
                             success: function(result) {
                                 console.log(result);
-                                 window.location.href = "http://gamer-net.herokuapp.com/user/0";
+                                this.transitionToRoute('gamelist')
                             },
                             error: function(result){
                                 console.log(JSON.stringify(result));
