455a284dcaf60270378b0e1744c4e36cd95c8bb5
wilx94 <wilson.velez1@upr.edu>
2015-11-08 22:37:23 -0400

    testing findAll(group)

--- app/routes/grouplist.js
+++ app/routes/grouplist.js
@@ -6,10 +6,10 @@ import Adapter from 'gamernet-ember-3/adapters/adapter';
 export default Ember.Route.extend({
     groups:undefined,
 	model: function(params) {
-		let adapter = Adapter.create();
-		let res = adapter.findAll("group").then(function(result){
-            console.log(JSON.stringify(result));
-    this.set('groups',result);
-    }.bind(this));
-        }
+            let adapter = Adapter.create();
+            let response = adapter.findAll("group").then(function(result){
+                    return result;                        
+                };)
+            return response;                            
+            }
 });
