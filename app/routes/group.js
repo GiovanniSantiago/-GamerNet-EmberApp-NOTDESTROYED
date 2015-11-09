e4ce2980df512c4684a041106390fb26b4c67206
wilx94 <wilson.velez1@upr.edu>
2015-11-09 14:44:40 -0400

    testing findAll(group)

--- app/routes/group.js
+++ app/routes/group.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
 	model: function(params) {
 		let adapter = Adapter.create();
 		console.log(params.group_id);
-		let res = adapter.find("group",params.group_id);
+		let res = adapter.findAll("group");
 		console.log(res);
 		return {group:res,groupPosts:adapter.accumCustom("post","owner_id",res.owner_id)};
 
--- app/routes/grouplist.js
+++ app/routes/grouplist.js
@@ -5,14 +5,15 @@ import Adapter from 'gamernet-ember-3/adapters/adapter';
 
 export default Ember.Route.extend({
    	model(){
-        let adapter = Adapter.create();
+            let adapter = Adapter.create();
             let response = adapter.findAll("group").then(function(rsut){
                 console.log(rsut);
                 return rsut;
             });
-        console.log("response: "+response);
         
-return response;
+            console.log("response: "+response);
+        
+            return response;
         
             
     } 
