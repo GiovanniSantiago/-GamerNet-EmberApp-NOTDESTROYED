ebd5e83804b1b5e6dcaddfa2b563a79c6927992c
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-15 21:56:53 -0400

    Fixing post sending

--- app/components/post-editor.js
+++ app/components/post-editor.js
@@ -11,19 +11,23 @@ export default Ember.Component.extend({
 		submitPost() {
 			let self = this;
 			console.log(this.get('title')+"\n"+this.get('body')+"\n"+this.get('authorId')+"\n"+this.get('ownerId'));
+
+			let data = {
+				title:self.get('title'),
+				text_body:self.get('body'),
+				author_id:self.get('authorId'),
+				post_owner_id:self.get('ownerId'),
+				post_category:"GENERAL",
+				datetime:"01100110"
+			};
+
+			console.log(data);
 			Ember.$.ajax({
 				type: "POST",
 				url: "//api-gamer-net.herokuapp.com/json/post",
 				processData: false,
 				contentType: 'application/json',
-				data: JSON.stringify({
-					title:self.get('title'),
-					text_body:self.get('body'),
-					author_id:self.get('authorId'),
-					post_owner_id:self.get('ownerId'),
-					post_category:"GENERAL",
-					datetime:"01100110"
-				})
+				data: JSON.stringify(data)
 			}).then(function(data) {
 				console.log(JSON.stringify(data));
 			});
