06d2089cf51dc24506f1f81b470a4850503a5d1c
wilx94 <wilson.velez1@upr.edu>
2015-11-17 19:19:14 -0400

    User list

--- app/routes/userlist.js
+++ app/routes/userlist.js
@@ -8,7 +8,7 @@ import App from 'gamernet-ember-3/models/generalClass';
 export default Ember.Route.extend({
    	model(){
             let adapter = Adapter.create();
-            let response = adapter.findAll("users",{user_id:App.user_id}).then(function(rsut){
+            let response = adapter.findAll("users").then(function(rsut){
                 console.log(rsut);
                 return rsut;
             });
