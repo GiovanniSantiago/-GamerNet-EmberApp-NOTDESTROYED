1fcd06c8c6e6c70c7e1b2d231dce30cba7fb2df9
wilx94 <wilson.velez1@upr.edu>
2015-11-17 22:48:43 -0400

    testing

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
-import App from 'gamernet-ember-3/models/generalClass';
-
-
-
 
 export default Ember.Route.extend({
-    model(){
-        let user_id=App.user_id;
-    }
 });
