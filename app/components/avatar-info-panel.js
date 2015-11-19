608e39d4462fc661d299f2eb1b8c519fc8ee28d9
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-18 22:49:30 -0400

    ASDSA

--- app/components/avatar-info-panel.js
+++ app/components/avatar-info-panel.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
 classNames:['panel', 'panel-default'],
 	actions: {
 		submitFriend() {
+			console.log("infopane.submitFriend called");
 			this.sendAction('submitFriend');
 		}
 	}
