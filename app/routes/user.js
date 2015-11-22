dfac8f6a93b896d12eb08126cb18f250852b1362
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-22 16:15:27 -0400

    ASLKDJ

--- app/routes/user.js
+++ app/routes/user.js
@@ -15,7 +15,7 @@ export default Ember.Route.extend({
 			dat.full_name=dat.first_name + dat.middle_name + dat.last_name;
 			dat.gamelists = adapter.findUserGamelists(params.user_id);
 			return adapter.findOwnedPosts(dat.post_owner_id).then(function(posts) {
-				return adapter.findFriends(App.user_id).then(function(friends) {
+				return adapter.findFriends(params.user_id).then(function(friends) {
 					dat.friends = friends;
 					return Ember.Object.create({user_id:App.user_id,user:dat,userPosts:posts});
 				});
