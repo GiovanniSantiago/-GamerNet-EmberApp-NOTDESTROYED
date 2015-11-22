505c189e81442bfe4c041e611ed61d297c27f590
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-22 18:10:06 -0400

    ASLKDJ

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -2,16 +2,19 @@ import Ember from 'ember';
 import Adapter from 'gamernet-ember-3/adapters/adapter';
 import General from 'gamernet-ember-3/models/generalClass';
 
+var adapter = Adapter.create();
+
 export default Ember.Controller.extend({
 	actions:{
 		addOwn() {
-			Adapter.addGamelistItem(General.user_id,"OWN",this.get('model').game_id);
+
+			adapter.addGamelistItem(General.user_id,"OWN",this.get('model').game_id);
 		},
 		addWant() {
-			Adapter.addGamelistItem(General.user_id,"WANT",this.get('model').game_id);
+			adapter.addGamelistItem(General.user_id,"WANT",this.get('model').game_id);
 		},
 		addLike() {
-			Adapter.addGamelistItem(General.user_id,"LIKE",this.get('model').game_id);
+			adapter.addGamelistItem(General.user_id,"LIKE",this.get('model').game_id);
 		}
 	}
 });
