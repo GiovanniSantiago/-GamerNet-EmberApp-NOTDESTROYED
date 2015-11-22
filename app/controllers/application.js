39c3aa612d1171d67ecb1c745c46129027aadd86
wilx94 <wilson.velez1@upr.edu>
2015-11-22 15:29:10 -0400

    tesing my outside view

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';  
-import App from 'gamernet-ember-3/models/generalClass';
+
 
 export default Ember.Controller.extend({
     firstName: 'Wilson',
     lastName: 'Velez',
-    user_id: App.user_id
+    
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import App from 'gamernet-ember-3/models/generalClass';
 
 export default Ember.Route.extend({
-  
+  model: function(parameters){
+	  return Ember.Object.create({user_id:App.user_id});
+  }
 });
