5f476c552cb813829a9ce2bdf27b93e24573ecb8
wilx94 <wilson.velez1@upr.edu>
2015-11-23 10:19:35 -0400

    asdfasdf

--- app/routes/group.js
+++ app/routes/group.js
@@ -11,7 +11,10 @@ export default Ember.Route.extend({
 		let res = adapter.findPlain('group',params.group_id);
 		return res.then(function(groupData) {
 			return adapter.findOwnedPosts(groupData.post_owner_id).then(function(posts) {
-				return Ember.Object.create({group:groupData,groupPosts:posts});
+				return adapter.findAll("/group/getUsersOf/"+params.group_id).then(function(membersOf){
+					return Ember.Object.create({group:groupData,groupPosts:posts,groupMembers:membersOf});
+				})
+				
 			});
 		});
 
