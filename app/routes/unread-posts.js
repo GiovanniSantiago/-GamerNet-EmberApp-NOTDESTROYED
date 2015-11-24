5f85f20f46148a4a58dde5c3ff2b277be3c338b4
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-11-24 17:29:15 -0400

    ASDA

--- app/routes/unread-posts.js
+++ app/routes/unread-posts.js
@@ -5,18 +5,14 @@ export default Ember.Route.extend({
 	model: function() {
 		var settings = {
 			type: "GET",
-			url: "//api-gamer-net.herokuapp.com/json/post/unread",
-			processData: false,
-			contentType: 'application/json'
+			url: "//api-gamer-net.herokuapp.com/json/post/unread"
 		};
 
 		var getPromise = Ember.$.ajax(settings);
 		return getPromise.then(function(dat) {
 			var delSettings = {
 				type: "DELETE",
-				url: "//api-gamer-net.herokuapp.com/json/post/unread",
-				processData: false,
-				contentType: 'application/json'
+				url: "//api-gamer-net.herokuapp.com/json/post/unread"
 			};
 			//Delete notified settings.
 			Ember.$.ajax(delSettings);
