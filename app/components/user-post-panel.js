b64f4ec8277c22e910f10a2be1a87797f1277dcf
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-12-09 13:53:02 -0400

    Attempt comments

--- app/components/user-post-panel.js
+++ app/components/user-post-panel.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames:["panel","panel-default"]
+	classNames:["panel","panel-default"],
+	actions: {
+		onComment(body,post_id) {
+			this.sendAction('onSubmitComment',self.get('body'), self.get('post_id'));
+		}
+	}
 });
